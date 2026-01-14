6f728266bf3c185f99d176b1b3e74cf16a0b611a
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-01-14 18:37:26 +0530

    first commit

---
+++ c programma/3Q.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1;
+    int num2;
+    scanf("%d" ,&num1);
+        scanf("%d",&num2);
+    int n = num1 + num2;
+    printf("The sum of %d and %d is %d." , num1 ,num2,n);
+    
+   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
