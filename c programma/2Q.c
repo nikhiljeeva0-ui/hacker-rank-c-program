c23412987b6f6fe28cf1d424d3d9de799a8223b8
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-01-14 18:34:17 +0530

    first commit

---
+++ c programma/2Q.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   char a; 
+    scanf("%c", &a);
+    char v;
+    v = a -32 ;
+    printf("The uppercase of %c is %c" ,a,v);
+    return 0;
+}
