91556d1415bcd0a8ed5b6a261a6c850650e4bbb9
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-01-21 19:03:03 +0530

    first commit

---
+++ c programma/Check if Two Numbers Have Same Last Digit  .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ int a , b;
+    scanf("%d %d",&a ,&b);
+    if(a%10==b%10){
+        printf("YES");
+        
+    }
+    else{
+        printf("NO");
+    }
+        
+    return 0;
+}
+
