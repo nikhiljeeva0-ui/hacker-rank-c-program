750e2469c498817c3746bf42d998d6f1f19f2057
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-01-25 19:49:07 +0530

    first commit

---
+++ c programma/Rounded Value - Remove Decimals.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float value ;
+    scanf("%f",&value);
+    int val = value;
+    printf("Rounded Value = %d" , val);
+    return 0;
+}
