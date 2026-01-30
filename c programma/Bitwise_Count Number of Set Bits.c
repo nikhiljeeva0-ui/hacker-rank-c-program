85231ca6b23ce6cc1a06aa75191fc71d9c66bd86
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-01-30 23:20:48 +0530

    first commit

---
+++ c programma/Bitwise_Count Number of Set Bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+        int n;
+    scanf("%d", &n);
+
+    int count = 0;
+
+    if (n & 1) {
+        count++;
+    }
+    if (n & 2){
+        count++;
+    } 
+    if (n & 4){
+        count++;
+    } 
+    if (n & 8){
+         count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
