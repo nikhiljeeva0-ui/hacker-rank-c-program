a62211c7973704b624f047b9063faeb705564829
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-02-09 07:19:27 +0530

    first commit

---
+++ c programma/switch/If-Else_Smallest Even Multiple  .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   int n;
+    scanf("%d",&n);
+     if(n%2== 0){
+      printf("%d",n);
+     }
+    else{
+    
+        printf("%d",n *2);
+    }
+    
+    return 0;
+}
