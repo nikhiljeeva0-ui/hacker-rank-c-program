f8ed61473b06d1dc1eb6d4bc65359cbbe6ccea25
nikhiljeeva0-ui <nikhiljeeva0@gmail.com>
2026-03-02 21:12:00 +0530

    first commit

---
+++ c programma/Loops_FizzBuzz Sequence.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        if (i % 15 == 0) {
+            printf("FizzBuzz\n");
+        } 
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } 
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } 
+        else {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
